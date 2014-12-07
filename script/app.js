b82e05a60eb4417751df8429948807c470af2e71
francois_blanchard <contact@fblanchard.fr>
2014-12-07 19:28:00 +0100

    add angular script

--- script/app.js
+++ script/app.js
@@ -1,14 +1,43 @@
 var app = angular.module('christmas',[]);
 
-app.controller('UsersController', ['$scope',function($scope){
-
-  $scope.users = [
+app.service('User', function(){
+  $this = this
+  $this.users = [
     {
-      "username": "Test1"
+      "username": "François",
+      "picked": false
     },
     {
-      "username": "Test2"
+      "username": "Émilie",
+      "picked": false
     }
   ];
+  $this.all = function(){
+    return $this.users;
+  };
+});
 
-}]);
+app.controller('UsersController', function($scope, User){
+
+  // STATUS : 'init','pick','end'
+  $scope.users = User.all();
+  $scope.newUser = {};
+  $scope.pick_status = "pick";
+
+  $scope.addUser = function(){
+    $scope.newUser.picked = false;
+    $scope.users.push($scope.newUser);
+    $scope.newUser = {};
+    $scope.pick_status = "pick";
+  };
+
+  $scope.pick_random_user = function(){
+    var remaining_user = _.where($scope.users, { 'picked': false });
+    $scope.user_picked = remaining_user[_.random(remaining_user.length-1)];
+    $scope.user_picked.picked = true;
+    if(remaining_user.length == 1){
+      $scope.pick_status = "end";
+    }
+  };
+
+});
